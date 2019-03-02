12e9213d1ca59a9c23b2f8037d0e142cfd24acbe
davio07 <39504202+davio07@users.noreply.github.com>
2019-03-02 21:43:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openContact(){
+  document.getElementById('contact').style.display= 'flex';
+}
+function closeContact(){
+  document.getElementById('contact').style.display= 'none';
+}
+function openMenu(){
+  document.getElementById('navmobile').style.display= 'flex';
+}
+function closeMenu(){
+  document.getElementById('navmobile').style.display= 'none';
+}
